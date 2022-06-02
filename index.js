07d220fc3b19518588e68e465686e2a70dd88d03
Ashraf Said <70977697+996as@users.noreply.github.com>
2022-06-02 22:51:59 +0100

    member welcome message stuffs

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Discord = require("discord.js"); //imports discord.js package
 require("dotenv").config();
 
+const generateImage = require("./generateImage");
+
 //creates bot client - used to access discord api
 const client = new Discord.Client({ 
     intents: [ //things to look out for
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 }); 
 
@@ -18,6 +21,16 @@ client.on("messageCreate", (message) => {
     if (message.content == "hi") {
         message.reply("Hello World!");
     }
+});
+
+const welcomeChannelID = "982009784305860678";
+//new member welcome message
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member);
+    member.guild.channels.cache.get(welcomeChannelID).send({
+       content: `<@${member.id}> Welcome to the server!`,
+       files: [img]
+    })
 });
 
 client.login(process.env.TOKEN); //login to bot
