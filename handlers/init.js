aba86f7eaef90961a729c2359b8d002da2a97f2b
Ashraf Said <70977697+996as@users.noreply.github.com>
2022-06-26 18:12:10 +0100

    hotfix

--- handlers/init.js
+++ handlers/init.js
@@ -1,24 +1,28 @@
 const {REST} = require("@discordjs/rest");
 const {Routes} = require("discord-api-types/v9");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+
+dotenv.config();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+const TOKEN = process.env.TOKEN;
+const MONGOOSE_STRING = process.env.MONGOOSE_STRING;
 
 const init = (client) => {
     const commandsData = [
         ...Array.from(client.commands.values()).map((c) => c.data.toJSON()),
     ]
 
-    const rest = new REST({version: "9"}).setToken(process.env.TOKEN);
+    const rest = new REST({version: "9"}).setToken(TOKEN);
 
     (async () => {
         try {
             console.log("refreshing application (/) commands.");
-            await rest.put(Routes
-                .applicationGuildCommands(
-                    process.env.CLIENT_ID, 
-                    process.env.GUILD_ID), 
-                    {body: commandsData})
+            await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commandsData});
             console.log("successfully reloaded application (/) commands.");
-            mongoose.connect(process.env.MONGOOSE_STRING, {
+            mongoose.connect(MONGOOSE_STRING, {
                 useUnifiedTopology: true,
                 useNewUrlParser: true
             }).then(() => console.log("connected to mongodb."))
