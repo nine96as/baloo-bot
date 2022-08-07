abd04df843bb9c207c20d13d9ed4a8fc220b9144
Ashraf Said <70977697+996as@users.noreply.github.com>
2022-08-07 18:54:48 +0100

    context menus additions; user info tweaks

---
+++ src/commands/context/getUserInfo.js
@@ -0,0 +1,65 @@
+/* eslint-disable require-jsdoc */
+import {
+  ContextMenuCommandBuilder,
+  EmbedBuilder,
+  ApplicationCommandType,
+  time
+} from 'discord.js';
+
+export const data = new ContextMenuCommandBuilder()
+    .setName('getUserInfo')
+    .setType(ApplicationCommandType.User);
+
+export async function execute(interaction) {
+  const member = interaction.targetMember;
+
+  const embed = new EmbedBuilder()
+      .setColor("Random")
+      .setAuthor({
+        iconURL: member.user.displayAvatarURL(),
+        name: member.user.tag,
+      })
+      .setDescription(`${member.user}`)
+      .setFooter({
+        text: `ID: ${member.user.id}`,
+      })
+      .setTimestamp()
+      .setThumbnail(
+          member.user.avatarURL({size: 2048, dynamic: true}),
+      )
+      .addFields(
+          {
+            name: 'joined',
+            value: `${time(member.joinedAt)} ` +
+            `(${time(member.joinedAt, 'R')})`,
+          },
+          {
+            name: 'registered',
+            value: `${time(member.user.createdAt)} ` +
+            `(${time(member.user.createdAt, 'R')})`,
+          },
+          {
+            name: 'roles',
+            value: `${
+              member.roles.cache
+                  .map((r) => r)
+                  .join(' ')
+                  .replace('@everyone', '') || 'none'
+            }`,
+          },
+          {
+            name: 'permissions',
+            value: `${
+              member.permissions
+                  .toArray()
+                  .map((p) => p.toLowerCase())
+                  .join(', ')
+                  .replaceAll('_', ' ') || 'none'
+            }`,
+          },
+      );
+
+  await interaction.reply({
+    embeds: [embed],
+  });
+}
